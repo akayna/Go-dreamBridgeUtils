68d41c77cab362bd91862952f8bc363f4f17ad83
Rafael Sonhador <aquinocunha@gmail.com>
2022-02-24 17:31:01 -0300

    Removed Cybersource's signature generation.

--- digest/digest.go
+++ digest/digest.go
@@ -1,7 +1,6 @@
 package digest
 
 import (
-	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
 	"log"
@@ -26,22 +25,4 @@ func GenerateDigest(data string) (string, error) {
 	h.Write(dataUTF8)
 
 	return base64.StdEncoding.EncodeToString(h.Sum(nil)), nil
-}
-
-// GenerateSignature - Generate the CyberSource signature using the key and data
-func GenerateSignature(key, data string) (string, error) {
-
-	decodedKey, err := base64.StdEncoding.DecodeString(key)
-	if err != nil {
-		log.Println("digest - GenerateSignature: Error decoding key from Base64.")
-		log.Println("error:", err)
-		return "", err
-	}
-
-	hasher := hmac.New(sha256.New, decodedKey)
-	hasher.Write([]byte(data))
-
-	signatureEncodedSTD := base64.StdEncoding.EncodeToString(hasher.Sum(nil))
-
-	return signatureEncodedSTD, nil
 }
